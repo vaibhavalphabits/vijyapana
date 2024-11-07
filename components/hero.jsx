5b5200d021b411cfe489d05f5f683e3d465ed16c
imabhaysachan20 <150459720+imabhaysachan20@users.noreply.github.com>
2024-11-07 20:53:49 +0530

    add hero images

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,6 +1,7 @@
 'use client';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Autoplay from "embla-carousel-autoplay"
+
 import {
     Carousel,
     CarouselContent,
@@ -15,30 +16,49 @@ import {
   
 import Image from 'next/image'
 import TalkToUsBTN from './TalkToUsBTN';
+import { getHeroImages } from '@/lib/contentful';
+import { Skeleton } from './ui/skeleton';
 function hero() {
+  const [images,setImages] = useState([])
+  useEffect(()=>{
+    async function get(){
+    const res = await getHeroImages();
+    setImages(res[0].images)
+  }
+  
+  
+  get()
+},[])
+
+  if (images.length===0) {
+    return <div className="h-screen w-full flex items-center justify-center bg-gray-100">
+    <Skeleton className="w-full h-full" />
+  </div>
+  }
   return (
     <Carousel opts={{
       loop:true
     }} plugins={[
       Autoplay({
         delay: 3000,
-      }),]} className="h-screen mt-[106px] md:mt-[64px]">
+      }),]} className="h-screen mt-[106px] md:mt-[64px] border">
   <CarouselContent>
-    <CarouselItem >
-        <div className='h-screen relative '>
-            <Image src={img1} className='w-full h-full object-cover md:object-top'  alt="hero"></Image>
-            </div>
-    </CarouselItem>
-    <CarouselItem>
-    <div className='h-screen relative '>
-    <Image src={img1} className='w-full h-full object-cover md:object-top'  alt="hero"></Image>
-            </div>
-    </CarouselItem>
-    <CarouselItem>
-    <div className='h-screen relative '>
-    <Image src={img1} className='w-full h-full object-cover md:object-top'  alt="hero"></Image>
-    </div>
-    </CarouselItem>
+    {images.map((img)=>{
+      return <CarouselItem >
+      <div className='h-screen relative '>
+      <Image 
+  src={img} 
+  
+  width={1200} 
+  height={700} 
+  quality={100} 
+  className="w-full h-full object-cover md:object-center" 
+  alt="hero" 
+/>          </div>
+  </CarouselItem>
+    })}
+    
+    
   </CarouselContent>
   <CarouselPrevious />
   <CarouselNext />
