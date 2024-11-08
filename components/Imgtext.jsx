11c78657cd7378a5554f6d873cf5a881e8e60817
imabhaysachan20 <150459720+imabhaysachan20@users.noreply.github.com>
2024-11-08 13:47:19 +0530

    remove image container

--- components/Imgtext.jsx
+++ components/Imgtext.jsx
@@ -5,13 +5,13 @@ import Container from './container'
 
 function Imgtext({text}) {
   return (
-    <Container>
+    
     <div style={{backgroundImage:"url(images/bg-img.jpg)", backgroundSize:"cover", backgroundAttachment:"fixed",backgroundPosition:"center"}} className='mt-36 w-full border h-[40vh] relative'>
       <div className="flex items-center justify-center absolute inset-0 bg-[#ffffff75]">
     <h1 className='uppercase text-md md:text-3xl tracking-wider font-bold'>{text}</h1>
       </div>
     </div>
-    </Container>
+    
   )
 }
 
