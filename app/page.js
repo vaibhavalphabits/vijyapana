5e803b14f38957ef228eeaf21b0611039e47f3e9
Vaibhav Shukla <shuklavaibhav267@gmail.com>
2024-11-08 16:30:37 +0530

    Added Text Design

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     
     <Hero/>
 <Products/>
-   <Imgtext text="We have creative solution<br />for every Business needs."/>
+   <Imgtext text="creative solutions<br />for every Business needs."/>
     <Services/>
     <ImageSlideShow/>
     </>
